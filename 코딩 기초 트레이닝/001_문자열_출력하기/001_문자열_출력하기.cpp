cde01f82af3664d50dfb577c82bb5369568451b8
ffeff <acvo2703@gmail.com>
2024-02-28 06:28:22 +0900

    초기 커밋.

---
+++ 코딩 기초 트레이닝/001_문자열_출력하기/001_문자열_출력하기.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    int length = str.length();
+    if (length >= 1 && length <= 1000000)
+    {
+        for (int i = 0; i < length; i++) {
+            if (isspace(str[i]) == 0) {
+                cout << str[i];
+            }
+        }
+    }
+    return 0;
+}
