be88c313d2af9ac50f2791d2dc08c145c861e1fb
ffeff <acvo2703@gmail.com>
2024-03-02 22:28:40 +0900

    003,004,005 작성

---
+++ 코딩 기초 트레이닝/003_문자열 반복해서 출력하기/003_문자열 반복해서 출력하기.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    int n;
+    cin >> str >> n;
+    while (n--) {
+        cout << str;
+    }
+
+    //     int length = str.length();
+    //     if(1 <= length && length <= 10){
+    //         if(1 <= n && n <=5){
+    //            while(n--){
+    //                 cout << str;
+    //             }
+
+    //         }
+    //         else{
+    //             cout << "error";
+    //         }
+    //     }
+    //     else{
+    //         cout << "error";
+    //     }
+    return 0;
+}
---
+++ 코딩 기초 트레이닝/004_대소문자 바꿔서 출력하기/004_대소문자 바꿔서 출력하기.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+
+    int length = str.length();
+
+    if (1 <= length && length <= 20) {
+        for (int i = 0; i < str.size(); i++) {
+            if (isupper(str[i])) {
+                str[i] = tolower(str[i]);
+            }
+            else if (islower(str[i])) {
+                str[i] = toupper(str[i]);
+            }
+        }
+    }
+    cout << str << endl;
+    return 0;
+}
